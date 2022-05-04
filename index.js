6c08d3dc517655888ccba2b4216e30c14aaaef7d
raul reymundi <kinskispiral@gmail.com>
2022-05-04 15:57:50 -0400

    commit tree 551a6e712d51800186142afa128f7a64108dca79

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Code your solution in this file!
+var hqLocation = 42;
+
+function distanceFromHqInBlocks(someNumber){
+    return Math.abs(someNumber - hqLocation);
+}
+
+// distanceFromHqInBlocks(43);
+
+// distanceFromHqInBlocks(50);
+
+// distanceFromHqInBlocks(34);
+
+function distanceFromHqInFeet(someNumber){
+    let blockDistance = distanceFromHqInBlocks(someNumber);
+    return blockDistance * 264;
+}
+
+function distanceTravelledInFeet(start, destination){
+    return Math.abs(start - destination) * 264;
+}
+
+function calculatesFarePrice(start, destination){
+    let fareCost = distanceTravelledInFeet(start, destination); 
+        if (fareCost <= 400){
+            return fareCost * 0;
+        }
+            else if (fareCost > 400 && fareCost <= 2000){
+                return (fareCost - 400) * 0.02;
+            }
+                else if (fareCost > 2000 && fareCost <= 2500){
+                    return 25;
+                }
+                    else if (fareCost > 2500){
+                        return `cannot travel that far`;
+                    }
+}
+
